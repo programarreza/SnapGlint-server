c342ef529872ff8a6dd5a4c62f9e09fd2e55d7bf
programarreza <programarreza@gmail.com>
2024-01-28 19:39:22 +0600

    create a blog post api

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require("express");
 const mysql = require("mysql");
 require("dotenv").config();
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 8081;
 
 const app = express();
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 const db = mysql.createConnection({
   host: "localhost",
@@ -36,16 +37,42 @@ app.post("/signup", (req, res) => {
 // login post api
 app.post("/login", (req, res) => {
   const sql = "SELECT * FROM login WHERE `email` = ? AND `password` = ?";
-
   db.query(sql, [req.body.email, req.body.password], (err, data) => {
     if (err) {
       return res.json("Error");
     }
-    if(data.length > 0){
-      return res.json("Success")
-    }else{
-      return res.json("Fail")
+    if (data.length > 0) {
+      const id = data[0].id;
+      const token = jwt.sign({ id }, "jwtSecretKey", { expiresIn: 365 });
+      return res.json({
+        Login: true,
+        token,
+        user: {
+          userId: data[0].id,
+          name: data[0].name,
+          email: data[0].email,
+          image: data[0].image,
+        },
+      });
+    } else {
+      return res.json("Fail");
     }
+  });
+});
+
+// add blog api
+app.post("/add_blog", (req, res) => {
+  const sql =
+    "INSERT INTO blog (`userId`, `title`, `description`, `image`) VALUE (?)";
+  const values = [
+    req.body.userId,
+    req.body.title,
+    req.body.description,
+    req.body.image,
+  ];
+  db.query(sql, [values], (err, result) => {
+    if (err) return res.json(err);
+    return res.json(result);
   });
 });
 
